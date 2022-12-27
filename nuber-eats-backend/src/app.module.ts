823ea9c923db63e57f485df6c2ceb577dfc4441f
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2022-12-27 13:39:25 +0900

    remove restaurant module + add tesseract(OCR)

--- nuber-eats-backend/src/app.module.ts
+++ nuber-eats-backend/src/app.module.ts
@@ -6,30 +6,30 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { RestaurantModule } from './restaurant/restaurant.module';
-import { Restaurant } from './restaurant/entities/restaurant.entity';
+import { OCR } from './util/OCR';
+import { ConfigModule } from '@nestjs/config';
+import Tesseract from 'tesseract.js';
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5433,
-      username: "oreoTaste",
-      password: "1234",
-      database: "nuber-eats",
-      synchronize: true,
-      logging: true,
-      entities: [Restaurant],
-      subscribers: [],
-      migrations: [],
-  }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      playground: true}),
-    RestaurantModule,
-  ],
+  // imports: [
+  //   TypeOrmModule.forRoot({
+  //     type: "postgres",
+  //     host: "localhost",
+  //     port: 5433,
+  //     username: "oreoTaste",
+  //     password: "1234",
+  //     database: "nuber-eats",
+  //     synchronize: true,
+  //     logging: true,
+  //     entities: [],
+  //     subscribers: [],
+  //     migrations: [],
+  // }),
+  //   GraphQLModule.forRoot<ApolloDriverConfig>({
+  //     driver: ApolloDriver,
+  //     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+  //     playground: true}),
+  // ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, OCR],
 })
 export class AppModule {}
---
+++ nuber-eats-backend/src/util/OCR.ts
@@ -0,0 +1,28 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Worker, createWorker } from 'tesseract.js';
+
+@Injectable()
+export class OCR {
+    /*
+    How to use :
+    1. constructor
+    constructor(private readonly appService: AppService, private readonly ocr: OCR) {}
+    2. use in method
+    let rslt = await this.ocr.getTextFromPic('./eng_bw.png');
+    */
+    private worker : Worker;
+    constructor(){
+        createWorker().then(el => this.worker = el);
+    }
+    async close() {
+        this.worker.terminate();
+    }
+    async getTextFromPic(path : string) : Promise<string>{
+        let text = "";
+        await this.worker.loadLanguage('eng+kor');
+        await this.worker.initialize('eng+kor');
+    
+        await this.worker.recognize(path).then(el => text = el.data.text);
+        return text;
+    };
+}
